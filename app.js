e17bac7dff9c04816aff1178c3fe0a54ea5545fa
Vinicius Silva <vinicius768@msn.com>
2018-10-16 17:35:23 -0300

    Development

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const request = require('request');
 
-request.get('https://www.viniciusls.com.br', (err, res, body) => console.log(err, res.statusCode, body));
+request.get('https://www.viniciusls.com.br', (err, res, body) => {
+    if (res.statusCode === 200)
+        return;
+
+    // Call telegram bot to send message alerting that the application is down!
+});
