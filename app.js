3624f38844bb8b6f97561a176abd62949db1e23b
Vinicius Silva <vinicius768@msn.com>
2018-10-17 19:27:56 -0300

    Changes to use with AWS Lambda

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ const ini = require('ini');
 const request = require('request');
 const Telegram = require('telegraf/telegram');
 
-const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-const telegramConfig = ini.parse(fs.readFileSync('./telegram.ini', 'utf-8'));
+exports.handler = async (event) => {
+    const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+    const telegramConfig = ini.parse(fs.readFileSync('./telegram.ini', 'utf-8'));
 
-const telegram = new Telegram(telegramConfig.token);
+    const telegram = new Telegram(telegramConfig.token);
 
-const urls = config.monitor.urls;
+    const urls = config.monitor.urls;
 
-for (const url of urls) {
-    request.get(url, (err, res, body) => {
-        if (res && res.statusCode === 200) {
-            return;
-        }
+    for (const url of urls) {
+        request.get(url, (err, res, body) => {
+            if (res && res.statusCode === 200) {
+                return;
+            }
 
-        const date = new Date();
+            const date = new Date();
 
-        const message = `The application located at ${url} is DOWN on ${date}!`;
-        // Call telegram bot to send message alerting that the application is down!
-        telegram.sendMessage(telegramConfig.chatId, message)
-            .then()
-            .catch(() => {
-                throw new InternalError('Error on sending message! Message: ${message}');
-            });
-    });
-}
+            const message = `The application located at ${url} is DOWN on ${date}!`;
+            // Call telegram bot to send message alerting that the application is down!
+            telegram.sendMessage(telegramConfig.chatId, message)
+                .then()
+                .catch(() => {
+                    throw new InternalError('Error on sending message! Message: ${message}');
+                });
+        });
+    }
+};
