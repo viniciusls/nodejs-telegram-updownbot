4fcf34511945c426e59458c1b88ec9e17fe55141
Vinicius Silva <vinicius768@msn.com>
2018-10-17 10:00:06 -0300

    Iterate through urls configured in config.ini and request them

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
+const ini = require('ini');
 const request = require('request');
 
-request.get('https://www.viniciusls.com.br', (err, res, body) => {
-    if (res.statusCode === 200)
-        return;
+const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
-    // Call telegram bot to send message alerting that the application is down!
-});
+const urls = config.monitor.urls;
+
+for (const url of urls) {
+    request.get(url, (err, res, body) => {
+        if (res.statusCode === 200) {
+            return;
+        }
+
+        // Call telegram bot to send message alerting that the application is down!
+    });
+}
