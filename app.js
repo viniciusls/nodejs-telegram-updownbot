f7469a1485eb6e7b60e39ddf8861178e18ceed7b
Vinicius Silva <vinicius768@msn.com>
2018-10-17 10:36:40 -0300

    Development

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const ini = require('ini');
 const request = require('request');
+const Telegram = require('telegraf/telegram');
 
 const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+const telegramConfig = ini.parse(fs.readFileSync('./telegram.ini', 'utf-8'));
+
+const telegram = new Telegram(telegramConfig.token);
 
 const urls = config.monitor.urls;
 
@@ -12,6 +16,12 @@ for (const url of urls) {
             return;
         }
 
+        const message = 'The application located at ${url} is DOWN!';
         // Call telegram bot to send message alerting that the application is down!
+        telegram.sendMessage(telegramConfig.chatId, message)
+            .then()
+            .catch(() => {
+                throw new InternalError('Error on sending message! Message: ${message}');
+            });
     });
 }
