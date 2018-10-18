e7e2fa3a03ee4e2ca386b90876d5e6e9f100f4ec
Vinicius Silva <vinicius768@msn.com>
2018-10-18 10:57:03 -0300

    Changes to use with AWS Lambda

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ exports.handler = async (event) => {
 
     for (const url of urls) {
         request.get(url, (err, res, body) => {
+            console.log(`Trying to request ${url}...`);
+
             if (res && res.statusCode === 200) {
                 return;
             }
@@ -20,9 +22,13 @@ exports.handler = async (event) => {
             const date = new Date();
 
             const message = `The application located at ${url} is DOWN on ${date}!`;
+
+            console.log(message);
             // Call telegram bot to send message alerting that the application is down!
             telegram.sendMessage(telegramConfig.chatId, message)
-                .then()
+                .then(() => {
+                    console.log('Message sent to Telegram Bot');
+                })
                 .catch(() => {
                     throw new InternalError('Error on sending message! Message: ${message}');
                 });
